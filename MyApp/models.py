2446a9bbe27a7a6039e2152c365fc4a408721e76
JoshuaThai <joshua.thaisananikone@gmail.com>
2026-04-05 17:14:17 -0500

    Completed Application

--- MyApp/models.py
+++ MyApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+    week = models.CharField(max_length=14)
+    date_time = models.DateTimeField()
+    location = models.CharField(max_length=100)
--- MyApp/views.py
+++ MyApp/views.py
@@ -1,3 +1,26 @@
+from gc import get_objects
+
+from django.db.models import Model
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Ticket
 
 # Create your views here.
+def home(request):
+    if request.method == "POST":
+        dayOfWeek = request.POST.get("week")
+        date_time = request.POST.get("date-time")
+        location = request.POST.get("location")
+        # This immediately saves the new ticket to the database
+        new_ticket = Ticket.objects.create(
+            week=dayOfWeek,
+            date_time=date_time,
+            location=location
+        )
+        new_ticket.save()
+        tickets = Ticket.objects.all()
+        return render(request, "tickets.html", {"tickets": tickets})
+
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
